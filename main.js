09a2cb21ff7fb2d6196fa09bc6b38232c15024a4
Sasha2319 <hlushchenkooleksandr123@gmail.com>
2020-04-23 11:37:59 +0300

    if click alert Xpos and Ypos added style.css added positions.txt added who won the game

--- main.js
+++ main.js
@@ -3,11 +3,44 @@ let ctx = cvs.getContext("2d");
 
 let O = new Image();
 let X = new Image();
+let mark = new Audio();
+
+
+
+let res = [];
 
 
 O.src = "IMG/0.png"
 X.src = "IMG/X.png"
 
+
 X.onload = function(){
     ctx.drawImage(X, 0, 0)
+    ctx.drawImage(X, 0, 120);
+    ctx.drawImage(X, 0, 240)
 }
+
+
+
+$("#canvas").click(function(e) {
+    var offset = $(this).offset();
+    var relativeX = (e.pageX - offset.left);
+    var relativeY = (e.pageY - offset.top);
+   
+    alert("X: " + relativeX + "  Y: " + relativeY);
+  });
+
+
+
+//!начало кода проверки на выигрыш
+/*function isSolved(board) {
+    board = board.join('-').replace(/,/g,'');
+    console.log(board)
+    if(/222|2...2...2|2....2....2|2..2..2/.test(board)) return 2;
+    if(/111|1...1...1|1....1....1|1..1..1/.test(board)) return 1;
+    if(/0/.test(board)) return -1;
+    return 0;
+ }
+ 
+ isSolved(res);*/
+ //!конец кода проверки на выигрыш
