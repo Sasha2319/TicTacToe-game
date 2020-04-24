d382ae46f7cdb8ef01cefbd1f205857b83c488f0
Sasha2319 <hlushchenkooleksandr123@gmail.com>
2020-04-24 14:21:06 +0300

    Game was completedgit push -u origin master!

--- main.js
+++ main.js
@@ -3,21 +3,34 @@ let ctx = cvs.getContext("2d");
 
 let O = new Image();
 let X = new Image();
+let bg = new Image();
 let mark = new Audio();
+let player = true;
+//! True - X, False - O
+
+let res = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
+];
 
 
-
-let res = [];
-
-
-O.src = "IMG/0.png"
-X.src = "IMG/X.png"
+O.src = "IMG/0 (1).png"
+X.src = "IMG/X70-60.png"
+bg.src = "IMG/BG (1).png"
 
 
 X.onload = function(){
-    ctx.drawImage(X, 0, 0)
-    ctx.drawImage(X, 0, 120);
-    ctx.drawImage(X, 0, 240)
+  ctx.drawImage(bg, 0, 0)
+  //   ctx.drawImage(X, 0, 0);
+  //   ctx.drawImage(X, 0, 120);
+  //   ctx.drawImage(X, 0, 240);
+  //   ctx.drawImage(X, 120, 0);
+  //   ctx.drawImage(X, 240, 0);
+  //   ctx.drawImage(X, 120, 120);
+  //   ctx.drawImage(X, 240, 120);
+  //   ctx.drawImage(X, 120, 240);
+  //   ctx.drawImage(X, 240, 240)
 }
 
 
@@ -27,20 +40,125 @@ $("#canvas").click(function(e) {
     var relativeX = (e.pageX - offset.left);
     var relativeY = (e.pageY - offset.top);
    
-    alert("X: " + relativeX + "  Y: " + relativeY);
+    //*  alert("X: " + relativeX + "  Y: " + relativeY);
+     //! 1
+    if(relativeX > 0 && relativeX < 110 && relativeY > 0 && relativeY < 100){
+      if(player === true && res[0][0] === 0){
+        ctx.drawImage(X, 0, 0);
+        res[0][0] = 1;
+      } 
+      if(player === false && res[0][0] === 0){
+        ctx.drawImage(O, 0, 0);
+        res[0][0] = 2;
+      } 
+    }
+    //! 2
+    if(relativeX > 120 && relativeX < 230 && relativeY > 0 && relativeY < 100){
+      if(player === true && res[0][1] === 0){
+        ctx.drawImage(X, 120, 0);
+        res[0][1] = 1;
+      } 
+      if(player === false && res[0][1] === 0){
+        ctx.drawImage(O, 120, 0);
+        res[0][1] = 2;
+      } 
+    }
+    //!3
+    if(relativeX > 240 && relativeX < 345 && relativeY > 0 && relativeY < 100){
+      if(player === true && res[0][2] === 0){
+        ctx.drawImage(X, 240, 0);
+        res[0][2] = 1;
+      } 
+      if(player === false && res[0][2] === 0){
+        ctx.drawImage(O, 240, 0);
+        res[0][2] = 2;
+      } 
+    }
+    //!4
+    if(relativeX > 0 && relativeX < 120 && relativeY > 120 && relativeY < 215){
+      if(player === true && res[1][0] === 0){
+        ctx.drawImage(X, 0, 120);
+        res[1][0] = 1;
+      } 
+      if(player === false && res[1][0] === 0){
+        ctx.drawImage(O, 0, 120);
+        res[1][0] = 2;
+      } 
+    }
+    //!5
+    if(relativeX > 120 && relativeX < 230 && relativeY > 120 && relativeY < 215){
+      if(player === true && res[1][1] === 0){
+        ctx.drawImage(X, 120, 120);
+        res[1][1] = 1;
+      } 
+      if(player === false && res[1][1] === 0){
+        ctx.drawImage(O, 120, 120);
+        res[1][1] = 2;
+      } 
+    }
+    //!6
+    if(relativeX > 240 && relativeX < 345 && relativeY > 120 && relativeY < 215){
+      if(player === true && res[1][2] === 0){
+        ctx.drawImage(X, 240, 120);
+        res[1][2] = 1;
+      } 
+      if(player === false && res[1][2] === 0){
+        ctx.drawImage(O, 240, 120);
+        res[1][2] = 2;
+      } 
+    }
+    //!7
+    if(relativeX > 0 && relativeX < 120 && relativeY > 230 && relativeY < 330){
+      if(player === true && res[2][0] === 0){
+        ctx.drawImage(X, 0, 240);
+        res[2][0] = 1;
+      } 
+      if(player === false && res[2][0] === 0){
+        ctx.drawImage(O, 0, 240);
+        res[2][0] = 2;
+      } 
+    }
+    //!8
+    if(relativeX > 120 && relativeX < 230 && relativeY > 230 && relativeY < 330){
+      if(player === true && res[2][1] === 0){
+        ctx.drawImage(X, 120, 240);
+        res[2][1] = 1;
+      } 
+      if(player === false && res[2][1] === 0){
+        ctx.drawImage(O, 120, 240);
+        res[2][1] = 2;
+      } 
+    }
+    //!9
+    if(relativeX > 240 && relativeX < 345&& relativeY > 230 && relativeY < 330){
+      if(player === true && res[2][2] === 0){
+        ctx.drawImage(X, 240, 240);
+        res[2][2] = 1;
+      } 
+      if(player === false && res[2][2] === 0){
+        ctx.drawImage(O, 240, 240);
+        res[2][2] = 2;
+      } 
+    }
+    if(isSolved(res) === 1){
+      alert("X win")
+    };
+    if(isSolved(res) === 2){
+      alert("O win")
+    };
+    player = !player;
   });
 
 
 
 //!начало кода проверки на выигрыш
-/*function isSolved(board) {
+function isSolved(board) {
     board = board.join('-').replace(/,/g,'');
     console.log(board)
     if(/222|2...2...2|2....2....2|2..2..2/.test(board)) return 2;
     if(/111|1...1...1|1....1....1|1..1..1/.test(board)) return 1;
     if(/0/.test(board)) return -1;
     return 0;
- }
- 
- isSolved(res);*/
+}
+ //! 1 - X, 2 - O
  //!конец кода проверки на выигрыш
